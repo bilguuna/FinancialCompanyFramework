37e6fd98937834e3593b7b217458762b0d25ec37
Bilguun Ariunbold <bil.ariunbold@gmail.com>
2014-08-12 10:54:55 -0500

    commit

---
+++ FinancialApp/src/creditcard/models/CreditCardCustomer.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package creditcard.models;
+
+import framework.Customer;
+import framework.IParty;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author javkhlant
+ */
+public class CreditCardCustomer extends Customer {
+
+    private String ccNumber;
+    private Date expireDate;
+    private SimpleDateFormat dateFormat;
+
+    public CreditCardCustomer(String name, String street, String city, String state, 
+            String zip, String email, String ccNumber, Date expireDate) {
+        super(name, street, city, state, zip, email);
+        this.ccNumber = ccNumber;
+        this.expireDate = expireDate;
+        dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+    }
+    
+    /**
+     * @return the ccNumber
+     */
+    public String getCcNumber() {
+        return ccNumber;
+    }
+
+    /**
+     * @param ccNumber the ccNumber to set
+     */
+    public void setCcNumber(String ccNumber) {
+        this.ccNumber = ccNumber;
+    }
+
+    /**
+     * @return the expireDate
+     */
+    public Date getExpireDate() {
+        return expireDate;
+    }
+
+    /**
+     * @param expireDate the expireDate to set
+     */
+    public void setExpireDate(Date expireDate) {
+        this.expireDate = expireDate;
+    }
+    
+    @Override
+    public Object[] getData() {
+        return new Object[]{getName(), getCcNumber(), dateFormat.format(getExpireDate()), "Credit Card", 0};
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" + "name=" + getName() + ", street=" + getStreet() + ", city=" 
+                + getCity() + ", state=" + getState() + ", zip=" + getZip() + ", email=" 
+                + getEmail() + ", ccNumber=" + getCcNumber() + ", ExpireDate=" + getExpireDate() + '}';
+    }
+}
