e60361561ada775da7363dd68a9804c25b71fa17
Bilguun Ariunbold <bil.ariunbold@gmail.com>
2014-08-12 16:45:57 -0500

    None changed

--- FinancialApp/src/bank/views/BankMainForm.java
+++ FinancialApp/src/bank/views/BankMainForm.java
@@ -5,12 +5,18 @@
  */
 package bank.views;
 
+import bank.models.CustomerCompany;
+import bank.models.CustomerPerson;
+import framework.Customer;
+import framework.IParty;
 import framework.Receiver;
 import framework.observer.Observer;
 import framework.view.DefaultAccountForm;
 import framework.view.MainForm;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Iterator;
+import java.util.Vector;
 import javax.swing.JButton;
 import javax.swing.JTable;
 
@@ -19,8 +25,10 @@ import javax.swing.JTable;
  * @author Bilguun
  */
 public class BankMainForm extends MainForm implements Observer {
+
     JButton JButton_PerAC = new javax.swing.JButton();
     JButton JButton_CompAC = new javax.swing.JButton();
+    JButton JButton_Addinterest = new javax.swing.JButton();
     private SymAction lSymAction = new SymAction();
     private Receiver receiver;
 
@@ -34,17 +42,28 @@ public class BankMainForm extends MainForm implements Observer {
 
     @Override
     public void updateView() {
-        refreshLIst();
+        model.setRowCount(0);
+        Vector<IParty> customers = receiver.getCustomerList();
+        Iterator it = customers.iterator();
+        while (it.hasNext()) {
+            Customer cus = (Customer) it.next();
+            model.addRow(cus.getData());
+        }
     }
-    
+
     class SymAction implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent event) {
-            DefaultAccountForm bankAccount = new BankAccountForm(invoker, receiver, "asd");
-            bankAccount.setBounds(450, 20, 300, 350);
-            bankAccount.setVisible(true);
-            bankAccount.dispose();
+            Object object = event.getSource();
+            if (object == JButton_Addinterest) {
+
+            } else {
+                DefaultAccountForm bankAccount = new BankAccountForm(invoker, receiver, ((JButton) object).getActionCommand());
+                bankAccount.setBounds(450, 20, 300, 350);
+                bankAccount.setVisible(true);
+                bankAccount.dispose();
+            }
             //refreshLIst();
         }
     }
@@ -63,12 +82,27 @@ public class BankMainForm extends MainForm implements Observer {
     @Override
     public void drawButtons() {
         JButton_PerAC.setText("Add personal account");
+        JButton_PerAC.setActionCommand("personal");
         JPanel1.add(JButton_PerAC);
         JButton_PerAC.setBounds(24, 20, 192, 33);
         JButton_CompAC.setText("Add company account");
+        JButton_CompAC.setActionCommand("company");
         JButton_CompAC.setActionCommand("jbutton");
         JPanel1.add(JButton_CompAC);
         JButton_CompAC.setBounds(240, 20, 192, 33);
+        JButton_Addinterest.setBounds(448, 20, 106, 33);
+        JButton_Addinterest.setText("Add interest");
+        JPanel1.add(JButton_Addinterest);
     }
-    
+
+//    @Override
+//    public void updateView() {
+//        model.setRowCount(0);
+//        Vector<IParty> customers = receiver.getCustomerList();
+//        Iterator it = customers.iterator();
+//        while (it.hasNext()) {
+//            Customer cus = (Customer) it.next();
+//            model.addRow(cus.getData());
+//        }
+//    }
 }
