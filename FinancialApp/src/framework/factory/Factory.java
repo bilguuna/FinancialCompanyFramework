8a6407a4629e1e96f8c7b0715331e5eb9a7bfbdb
Javkhlant <javkhlant.ch@gmail.com>
2014-08-12 00:20:58 -0500

    Revert "Merge branch 'master' of https://github.com/bilguuna/FinancialCompanyFramework"
    This reverts commit ff00aa8cb4ad46642300afeafba4a90380912587, reversing
    changes made to 9c95f8183ea612ab51a0216657202e28409aa022.

--- FinancialApp/src/framework/factory/Factory.java
+++ FinancialApp/src/framework/factory/Factory.java
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-package framework.factory;
-
-import framework.Account;
-import framework.Customer;
-import framework.DefaultAccount;
-import framework.view.DefaultAccountForm;
-import framework.DefaultCustomer;
-
-public class Factory extends AbstractFactory {
-
-    @Override
-    public Customer createCustomer(DefaultAccountForm defaultAcc) {
-        return new DefaultCustomer(defaultAcc.getName(), defaultAcc.getStreet(), defaultAcc.getCity(), defaultAcc.getState(), defaultAcc.getZip(), defaultAcc.getEmail(), new DefaultAccount(defaultAcc.getAccountNumber()));
-    }
-
-    @Override
-    public Account createAccount() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-}
-=======
 package framework.factory;
 
 import framework.Account;
@@ -44,4 +21,3 @@ public class Factory<T> extends AbstractFactory<T>{
     }
 
 }
->>>>>>> FETCH_HEAD
--- FinancialApp/src/framework/observer/Observer.java
+++ FinancialApp/src/framework/observer/Observer.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,22 +15,3 @@ public interface Observer {
     public void updateView();
     
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package framework.observer;
-
-/**
- *
- * @author Marlin
- */
-public interface Observer {
-    
-    public void updateView();
-    
-}
->>>>>>> origin/master
